e15126533a8e5669366395ac32f6dfda7dc1a0e5
CodeTivity08 <codetivity08@gmail.com>
2022-03-29 17:38:41 +0200

    First Python Commit

---
+++ Hello World/HelloWorld.py
@@ -0,0 +1,3 @@
+print('Hello, There!')
+person = input('What is your name?')
+print('Hello,', person)
---
+++ Hello World/alleyAlien.py
@@ -0,0 +1,4 @@
+name = 'Alley Alien'
+greeting = 'Welcome To Earth'
+message = greeting+name
+print = ('message')
